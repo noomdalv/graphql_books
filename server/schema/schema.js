4e1ddf41661008d0024f2728c45445650cd99ea4
noomdalv <vladlunasan@gmail.com>
2023-02-21 06:53:40 -0500

    tested graphiql endpoint

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,11 +1,17 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
+
+let books = [
+    { name: 'aaaa', genre: 'AAAAA', id: "1" },
+    { name: 'bbbb', genre: 'BBBBB', id: "2" },
+    { name: 'cccc', genre: 'CCCCC', id: "3" }
+];
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
         genre: { type: GraphQLString }
     })
@@ -16,10 +22,10 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         book: {
             type: BookType,
-            args: { id: { GraphQLString } },
+            args: { id: { type: GraphQLID } },
             resolve(parent, args) {
                 // code to get data from db / other source
-
+                return books.find(book => book.id === args.id);
             }
         }
     }
