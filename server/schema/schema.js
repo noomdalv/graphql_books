f3cfc261fa63c2334526718d01235d9e4b6e404a
noomdalv <vladlunasan@gmail.com>
2023-02-21 06:30:21 -0500

    added schema rootQuery setup

---
+++ server/schema/schema.js
@@ -0,0 +1,30 @@
+const graphql = require('graphql');
+
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    fields: () => ({
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString }
+    })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: { id: { GraphQLString } },
+            resolve(parent, args) {
+                // code to get data from db / other source
+
+            }
+        }
+    }
+});
+
+module.exports = new graphql.GraphQLSchema({
+    query: RootQuery
+});
